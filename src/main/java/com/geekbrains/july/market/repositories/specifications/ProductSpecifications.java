ba41556138fa411e0e19dcc085263d69bbedcbfa
Doom <lightfollower1985@gmail.com>
2020-07-18 20:10:03 +0500

    Деактивация листательных кнопок.

--- src/main/java/com/geekbrains/july/market/repositories/specifications/ProductSpecifications.java
+++ src/main/java/com/geekbrains/july/market/repositories/specifications/ProductSpecifications.java
@@ -5,14 +5,14 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class ProductSpecifications {
     public static Specification<Product> priceGreaterOrEqualsThan(int minPrice) {
-        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice);
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice);
     }
 
     public static Specification<Product> priceLesserOrEqualsThan(int maxPrice) {
-        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("price"), maxPrice);
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("price"), maxPrice);
     }
 
     public static Specification<Product> titleContainsFollowingExpression(String title) {
-        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("title"), "%" + title + "%");
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("title"), "%" + title + "%");
     }
 }
