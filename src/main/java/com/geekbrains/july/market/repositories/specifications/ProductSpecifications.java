4af864de7cf3acbaf1ba96b165b602f3ba919ac5
Doom <lightfollower1985@gmail.com>
2020-07-21 16:22:38 +0500

    Поиск по категориям

--- src/main/java/com/geekbrains/july/market/repositories/specifications/ProductSpecifications.java
+++ src/main/java/com/geekbrains/july/market/repositories/specifications/ProductSpecifications.java
@@ -1,18 +1,28 @@
 package com.geekbrains.july.market.repositories.specifications;
 
+import com.geekbrains.july.market.entities.Category;
 import com.geekbrains.july.market.entities.Product;
 import org.springframework.data.jpa.domain.Specification;
 
 public class ProductSpecifications {
     public static Specification<Product> priceGreaterOrEqualsThan(int minPrice) {
-        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice);
+        return (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice);
     }
 
     public static Specification<Product> priceLesserOrEqualsThan(int maxPrice) {
-        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("price"), maxPrice);
+        return (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.lessThanOrEqualTo(root.get("price"), maxPrice);
     }
 
     public static Specification<Product> titleContainsFollowingExpression(String title) {
-        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("title"), "%" + title + "%");
+        return (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.like(root.get("title"), "%" + title + "%");
+    }
+
+    public static Specification<Product> categoryIs(Category category){
+        return (root , criteriaQuery, criteriaBuilder) -> {
+            return criteriaBuilder.isMember(category, root.get("categories"));
+        };
     }
 }
