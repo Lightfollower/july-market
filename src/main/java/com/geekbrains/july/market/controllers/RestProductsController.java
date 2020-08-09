0d96358fe226c276df622b3159453533daf0506a
Doom <lightfollower1985@gmail.com>
2020-08-09 10:39:12 +0500

    Прикручиваю пагинацию и фильтры

--- src/main/java/com/geekbrains/july/market/controllers/RestProductsController.java
+++ src/main/java/com/geekbrains/july/market/controllers/RestProductsController.java
@@ -1,18 +1,23 @@
 package com.geekbrains.july.market.controllers;
 
+import com.geekbrains.july.market.entities.Category;
 import com.geekbrains.july.market.entities.Product;
 import com.geekbrains.july.market.entities.dtos.ProductDto;
 import com.geekbrains.july.market.exceptions.ProductNotFoundException;
+import com.geekbrains.july.market.services.CategoriesService;
 import com.geekbrains.july.market.services.ProductsService;
+import com.geekbrains.july.market.utils.ProductFilter;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
@@ -21,6 +26,7 @@ import java.util.List;
 @AllArgsConstructor
 public class RestProductsController {
     private ProductsService productsService;
+    private CategoriesService categoriesService;
 
     @GetMapping("/dto")
     @ApiOperation("Returns list of all products data transfer objects")
@@ -30,8 +36,18 @@ public class RestProductsController {
 
     @GetMapping(produces = "application/json")
     @ApiOperation("Returns list of all products")
-    public List<Product> getAllProducts() {
-        return productsService.findAll();
+    public List<Product> getAllProducts(@RequestParam Map<String, String> requestParams, @RequestParam(name = "categories", required = false) List<Long> categoriesIds) {
+        Integer pageNumber = Integer.parseInt(requestParams.getOrDefault("p", "1"));
+
+        List<Category> categoriesFilter = null;
+        if (categoriesIds != null) {
+            categoriesFilter = categoriesService.getCategoriesByIds(categoriesIds);
+        }
+        ProductFilter productFilter = new ProductFilter(requestParams, categoriesFilter);
+        Page<Product> products = productsService.findAll(productFilter.getSpec(), pageNumber);
+//        model.addAttribute("products", products);
+//        model.addAttribute("filterDef", productFilter.getFilterDefinition().toString());
+        return products.getContent();
     }
 
     @GetMapping(value = "/{id}", produces = "application/json")
