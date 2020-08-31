cdb6dde0a4e6ba6ea481b601b9c08f870a6d2940
Doom <lightfollower1985@gmail.com>
2020-08-31 18:12:35 +0500

    SOAP Controller

--- src/main/java/com/geekbrains/july/market/controllers/ProductEndpoint.java
+++ src/main/java/com/geekbrains/july/market/controllers/ProductEndpoint.java
@@ -1,13 +1,17 @@
-package com.geekbrains.july.market.configs;
+package com.geekbrains.july.market.controllers;
 
 import com.geekbrains.july.market.products.GetProductRequest;
 import com.geekbrains.july.market.products.GetProductResponse;
+import com.geekbrains.july.market.products.ProductSOAP;
 import com.geekbrains.july.market.services.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Endpoint
 public class ProductEndpoint {
@@ -23,11 +27,10 @@ public class ProductEndpoint {
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getProductRequest")
     @ResponsePayload
     public GetProductResponse getProduct(@RequestPayload GetProductRequest request) {
-        System.out.println(request.getTitle());
         GetProductResponse response = new GetProductResponse();
         System.out.println(response);
-        response.setProduct(productsService.findOneProductSOAPByTitle(request.getTitle()));
-        System.out.println(response.getProduct().getTitle());
+        List<ProductSOAP> products = new ArrayList<>();
+        response.setProduct(productsService.findAllSOAP());
         return response;
     }
 }
